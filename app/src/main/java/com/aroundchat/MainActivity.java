21068173d8244bd596480c87e5b5d8e6070226de
张家铭 <zhangjiaming@192.168.0.104>
2018-10-17 20:38:46 +0800

    文件目录调整

--- app/src/main/java/com/aroundchat/MainActivity.java
+++ app/src/main/java/com/aroundchat/MainActivity.java
@@ -1,7 +1,9 @@
-package chat.around.com.aroundchat;
+package com.aroundchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import chat.around.com.aroundchat.R;
 
 public class MainActivity extends AppCompatActivity {
 
